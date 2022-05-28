178ef29c08e64bcad41552f6c548fefe3ed151b8
junior <abelsouzacosta@gmail.com>
2022-05-28 03:39:49 -0300

    refactor(handler): adding the route handler
    The route handler proides a way to deal with the requests mades to the api

--- src/handler.mjs
+++ src/handler.mjs
@@ -1,11 +1,31 @@
+import { parse } from 'node:url'; 
+
+const allRoutes = {
+  '/': (request, response) => {
+    response.write("Hello World!!!");
+  },
+
+  '/heroes:get': (request, response) => {
+    response.write("Get all heroes in the database");
+    response.end();
+  },
+
+  default: (request, response) => {
+    response.writeHead(404, {
+      'content-type': 'application/json',
+    });
+    response.write("Not Found");
+    response.end();
+  }
+}
+
 function handler(request, response) {
   let { url, method } = request;
+  let { pathname } = parse(url, true);
+  let key = `${pathname}:${method.toLowerCase()}`
+  let chosen = allRoutes[key] || allRoutes.default
 
-  console.log({ url, method });
-
-  response.write('Hello World');
-
-  response.end();
+  return chosen(request, response)
 }
 
 export default handler;
